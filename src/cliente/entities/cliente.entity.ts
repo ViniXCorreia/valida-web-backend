0cd460707999056e4338bdb559d37526f7d53417
Vinicus Martins Correia <vinix.correia@gmail.com>
2022-08-24 01:11:42 -0300

    adjust entitys

--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { PropostaComercialEntity } from 'src/proposta-comercial/entities/proposta-comercial.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'cliente' })
 export class ClienteEntity {
@@ -10,4 +11,17 @@ export class ClienteEntity {
 
   @Column()
   document: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  //TODO RESPONSÁVEIS PELA EMPRESA
+
+  @OneToMany(() => PropostaComercialEntity, (proposta) => proposta.cliente, {
+    cascade: true,
+  })
+  propostas: PropostaComercialEntity[];
 }
--- src/proposta-comercial/entities/proposta-comercial.entity.ts
+++ src/proposta-comercial/entities/proposta-comercial.entity.ts
@@ -1,1 +1,26 @@
-export class PropostaComercial {}
+import { ClienteEntity } from 'src/cliente/entities/cliente.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'proposta-comercial' })
+export class PropostaComercialEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  createDate: Date;
+
+  @Column()
+  status: string;
+
+  @ManyToOne(() => ClienteEntity, (cliente) => cliente.propostas)
+  cliente: ClienteEntity;
+}
